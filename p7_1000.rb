27ffdd92a8efc12cbe3afa856a92da9d74b832d9
Jude Arasu <judearasu@gmail.com>
2013-01-14 00:09:37 +0530

    problem 7 soln 1

--- p7_1000.rb
+++ p7_1000.rb
@@ -1,25 +1,26 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
-def finding_primes(limit)
+def prime_range(limit)
 	list=2;
-	number=3
-	while number <=limit
-		number=number+2;
-		prime=true
-		x=3;
-        while(x<=Math.sqrt(number))
-        	x=x+2;
-        	if number%x==0
-        		prime = false;
-        		break;
-        	end
-        end               
-        if prime
-        	temp=number;
+	i=3
+    temp=[]
+    temp << list
+	i.step(limit,2) do |i|
+        prime=true
+        3.step(Math.sqrt(i),2) do |j|
+            if i%j==0
+               prime=false
+               break
+            end
         end
-        p temp
+    if prime==true
+       temp << i
     end
+    end
+   temp.flatten
 end
-
-p finding_primes(10)
-
+x=0
+until prime_range(x).length==100
+  x=x+1
+end
+p prime_range(x)
