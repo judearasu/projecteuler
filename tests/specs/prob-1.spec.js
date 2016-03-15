fe9bf7d85c6550a37270c66078e0b8d61b6c03a8
JudeArasu <judearasu@gmail.com>
2016-03-15 13:02:52 -0400

    sample test case

--- tests/specs/prob-1.spec.js
+++ tests/specs/prob-1.spec.js
@@ -1,7 +1,14 @@
 /*eslint-env node, mocha */
-describe('Problem one', function () {
+// describe('Problem one', function () {
+// 	describe('Internal Function1', function(){
+// 		expect('1').to.equal('1');
+// 	})
+// });
 
-	describe('Internal Function1', function(){
-		expect('1').to.equal('1');
-	})
+describe('Problem one', function() {
+  describe('Internal Methods', function () {
+    it('should be defined', function () {
+    	
+    });
+  });
 });
