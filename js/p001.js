e42488d59d53d83dbf698c485c310584e9a33a0d
JudeArasu <judearasu@gmail.com>
2016-03-21 00:00:48 -0400

    problem one resilved

--- js/p001.js
+++ js/p001.js
@@ -1,13 +1,45 @@
-(function(){
+/** Modular Patter **/
+// 	var euler001 = (function(){
+// 		'use strict';
+// 		var sum = 0;
+// 		var n = 1000;
+//
+// 		while(n--){
+// 			if(n%3 === 0 || n%5 ===0){
+// 				sum +=n;
+// 			}
+// 		}
+// 		console.log(sum);
+// 		return sum;
+// })();
+
+
+// var euler001 = (function(){
+// 	var soln ={};
+// 			soln.calculate = function(sum, n){
+// 				while(n--){
+// 					if(n%3 ===0 || n%5 ===0){
+// 						sum+=n;
+// 					}
+// 				}
+// 				console.log(sum);
+// 				return sum;
+// 			};
+// 			return soln;
+// })();
+
+var euler001 = (function() {
 	'use strict';
+  return {
+    calculate: getNaturalNumbers
+  };
 
-	 function ProblemOne(){
-	 	var vm = this;
-	 		vm.getValue = getValue;
-
-	 		function getValue(){
-	 			console.log('hi');
-	 		}
-	 }
-
+  function getNaturalNumbers(sum, n) {
+    while (n--) {
+      if (n % 3 === 0 || n % 5 === 0) {
+        sum += n;
+      }
+    }
+		return sum;
+  }
 })();
--- tests/specs/prob-1.spec.js
+++ tests/specs/prob-1.spec.js
@@ -5,10 +5,20 @@
 // 	})
 // });
 
-describe('Problem one', function() {
-  describe('Internal Methods', function () {
-    it('should be defined', function () {
-    	
+describe('Euler001', function() {
+  describe('Internal Methods', function() {
+    it('should be defined', function() {
+      expect(typeof euler001).to.be.equal("object")
+    });
+  });
+
+  describe('calcuate function', function() {
+    it('should accept two params', function() {
+      expect(euler001.calculate).to.have.length(2);
+    });
+
+    it('should return 23 when the natural numbers are below 10 and divisible by 3 or 5', function(){
+      expect(euler001.calculate(0,10)).to.be.equal(23);
     });
   });
 });
