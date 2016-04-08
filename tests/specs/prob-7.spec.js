c6f679d323b2b79e37889d3c183779bebe9f3712
JudeArasu <judearasu@gmail.com>
2016-04-08 00:36:19 -0400

    initial structure

---
+++ tests/specs/prob-7.spec.js
@@ -0,0 +1,10 @@
+/*eslint-env node, mocha */
+describe('Euler007', function() {
+  describe('Internal Methods', function() {
+    it('should be defined', function() {
+      expect(typeof euler007).to.be.equal("object")
+    });
+  });
+
+
+});
