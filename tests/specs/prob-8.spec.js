b80fa5e51c25dd7874031b06a043054613f12cce
JudeArasu <judearasu@gmail.com>
2016-04-23 16:40:47 -0400

    solved problem 8 by implementing multiplier separately

---
+++ tests/specs/prob-8.spec.js
@@ -0,0 +1,19 @@
+/*eslint-env node, mocha */
+describe.only('Euler008', function() {
+  describe('Internal Methods', function() {
+    it('should be defined', function() {
+      expect(typeof euler008).to.be.equal("object")
+    });
+  });
+
+  describe('getLargestProduct', function() {
+    it('should exist', function() {
+      expect(euler008.getLargestProduct).to.have.length(1);
+    });
+
+    it('should return the product value', function() {
+      expect(euler008.getLargestProduct(13)).to.be.equal(23514624000);
+    });
+
+  });
+});
